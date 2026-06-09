b2f6d68903d8e30e1bf82565909938e51fbead52
hendore <thenley28@googlemail.com>
2026-06-09 02:58:55 +0100

    Set media folder and pass in github branch by env var

--- tina/config.js
+++ tina/config.js
@@ -8,29 +8,19 @@ const branch =
   "main";
 
 export default defineConfig({
-  branch,
-  // Get this from tina.io
   clientId: process.env.PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
   token: process.env.TINA_TOKEN,
-
+  branch: process.env.GITHUB_BRANCH,
   build: {
     outputFolder: "admin",
     publicFolder: "public",
   },
-  // Uncomment to allow cross-origin requests from non-localhost origins
-  // during local development (e.g. GitHub Codespaces, Gitpod, Docker).
-  // Use 'private' to allow all private-network IPs (WSL2, Docker, etc.)
-  // server: {
-  //   allowedOrigins: ['https://your-codespace.github.dev'],
-  // },
   media: {
     tina: {
-      mediaRoot: "",
+      mediaRoot: "uploads",
       publicFolder: "public",
     },
   },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
   schema: {
     collections: [
       {
